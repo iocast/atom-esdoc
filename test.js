10493e85eede58b5848cca3b99a3e055b068e199
iocast <iocast@me.com>
2017-11-03 19:06:22 +0100

    added a test file

---
+++ test.js
@@ -0,0 +1,74 @@
+
+/**
+ * test
+ *
+ */
+export class test{
+
+  /**
+   *
+   *
+   * @param {type}	hello - this is the parameter hello
+   */
+  constructor(hello) {
+    this.name = hello;
+  }
+
+  /** @type {string} */
+  get hello() {
+    return "hello";
+  }
+
+  /** @type {number} */
+  get numb() {
+    return 123;
+  }
+
+  /** @type {} */
+  get name() {
+    return this.name;
+  }
+
+  /** @type {<type>} */
+  set name(value) {
+    this.name = value;
+  }
+
+  /**
+   *
+   *
+   * @param {type}	a - this is the parameter a
+   * @param {type}	b - this is the parameter b
+   * @param {type}	c - this is the parameter c
+   *
+   * @return String
+   */
+  calculate(a, b, c) {
+    return "";
+  }
+
+}
+
+
+/**
+ * test
+ *
+ * @param {type}	param1 - this is the parameter param1
+ */
+async function test(param1) {
+
+}
+
+/**
+ * test_func
+ *
+ * @param {type}	hello - this is the parameter hello
+ *
+ * @return String
+ */
+function test_func(hello) {
+  return "";
+}
+
+
+const TEST = "hello";
